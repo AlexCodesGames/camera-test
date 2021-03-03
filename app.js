816dc050298e906c18ddb3ffe7d48dbca22db46b
Alex Pazderp <alexPazder@hotmail.ca>
2021-03-03 11:25:54 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 // Set constraints for the video stream
 var constraints = { video: { facingMode: "user" }, audio: false };
 
-// Define constants
-const cameraView = document.querySelector("#camera--view")
-const cameraOutput = document.querySelector("#camera--output")
-const cameraSensor = document.querySelector("#camera--sensor")
-const cameraTrigger = document.querySelector("#camera--trigger")
+// Define constant links to document components
+//	action buttons
+const buttonConnect = document.querySelector("#buttonConnect")
+const buttonDisconnect = document.querySelector("#buttonDisconnect")
+const buttonSnapshot = document.querySelector("#buttonSnapshot")
+//	views
+const blockLoading = document.querySelector("#loadingBlock")
+const blockCamera = document.querySelector("#cameraBlock")
+const cameraView = document.querySelector("#cameraVideo")
+const cameraOutput = document.querySelector("#cameraOutput")
+const cameraSensor = document.querySelector("#cameraCanvas")
 	
 // Access the device camera and stream to cameraView
 function cameraStart() 
@@ -21,14 +27,39 @@ function cameraStart()
         console.error("Oops. Something is broken.", error);
     });
 }
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() 
+
+//attempts a connection to the device's camera
+buttonConnect.onclick = function() 
 {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
+	if(camera.style.display === "none")
+	{
+		blockLoading.style.display = "block";
+		blockCamera.style.display = "none";
+	}
+}
+//called when a connection has successfully been made and streaming has begun
+function successfulConnection() 
+{
+	blockLoading.style.display = "none";
+	blockCamera.style.display = "block";
+}
+
+//ends any pending or existing connection
+buttonDisconnect.onclick = function()
+{
+	blockLoading.style.display = "none";
+	blockCamera.style.display = "none";
+}
+
+// Take a picture when picture button is tapped
+buttonSnapshot.onclick = function() 
+{
+	cameraSensor.width = cameraView.videoWidth;
+	cameraSensor.height = cameraView.videoHeight;
+	cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+	cameraOutput.src = cameraSensor.toDataURL("image/webp");
+	cameraOutput.classList.add("taken");
 };
+
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
