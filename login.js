bde89b80fcae392ddf0513ee284c713f5955bf61
Alex Pazderp <alexPazder@hotmail.ca>
2021-03-05 12:21:43 -0800

    Update login.js

--- login.js
+++ login.js
@@ -1,10 +1,13 @@
 // Define constant links to document components
 //	text
 const textFeedback = document.querySelector("#textFeedback")
+const textFeedback = document.querySelector("#textFeedback")
 const buttonPassword = document.querySelector("#buttonPassword")
 //	connection
 var xhr;
 var rtcConnection;
+//
+var isPending = false;
 
 //used to initialize the webpage's entry state
 function initialize() 
@@ -19,22 +22,36 @@ function initialize()
 //attempts a connection to the doorbell to login
 function connectionAttempt()
 {
-    console.debug("sending login details...");
-		
-		//create a connection request
-		xhr = new XMLHttpRequest();
-		xhr.open('GET', "./PASSWORD-"+textClientIP.innerHTML, true);
-		xhr.send();
-		
-		xhr.addEventListener("readystatechange", connectionProcess, false);
+	console.debug("sending login details...");		
+
+	//create a connection request
+	xhr = new XMLHttpRequest();
+	xhr.open('GET', "./PASSWORD-"+buttonPassword.innerHTML, true);
+	xhr.send();
+
+	isPending = true;
+	
+	//add listener for response
+	xhr.addEventListener("readystatechange", connectionProcess, false);
+	//set timeout for no response
+	setTimeout(connectionTimeout, 4000);
+}
+//if there is a connection attempt pending but it times out
+function connectionTimeout()
+{
+	if(isPending == true)
+	{
+		textHostIP.innerHTML = "Response Timedout";
+		isPending = false
+	}
 }
 //processes returning connection details
 function connectionProcess(e)
 {
 	//check ready-state change type and success
 	if(xhr.readyState == 4 && xhr.status == 200)
-  {
-    console.debug("login response received");
+	{
+		console.debug("login response received");
 		//record host ip
 		textHostIP.innerHTML = xhr.responseText;
 		
@@ -44,7 +61,7 @@ function connectionProcess(e)
 		xhr.send();
 		
 		xhr.addEventListener("readystatechange", connectionSuccessful, false);
-  }
+	}
 	//if ready-state is finished but failed to aquire, disconnect
 	else if(xhr.readyState == 4)
 	{
@@ -52,7 +69,7 @@ function connectionProcess(e)
 	}
 }
 //set up button functionality
-buttonPassword.onclick = function() { connectionDisconnect() };
+buttonPassword.onclick = function() { connectionAttempt() };
 
 //sets initializer to activate when the webpage loads
 window.addEventListener("load", initialize, false);
