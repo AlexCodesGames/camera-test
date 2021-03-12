8f83eb6891a9bdb534da56a950d6855db4ed8f42
Alex Pazderp <alexPazder@hotmail.ca>
2021-03-11 19:27:43 -0800

    Update media.js

--- media.js
+++ media.js
@@ -12,16 +12,17 @@ const localVideo = document.querySelector('cameraVideo');
 let localStream;
 
 // Handles success by adding the MediaStream to the video element.
-function gotLocalMediaStream(mediaStream) {
+function gotLocalMediaStream(mediaStream) 
+{
   localStream = mediaStream;
   localVideo.srcObject = mediaStream;
 }
 
 // Handles error by logging a message to the console with the error message.
-function handleLocalMediaStreamError(error) {
+function handleLocalMediaStreamError(error) 
+{
   console.log('navigator.getUserMedia error: ', error);
 }
 
 // Initializes media stream.
-navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
-  .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+navigator.mediaDevices.getUserMedia(mediaStreamConstraints).then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
